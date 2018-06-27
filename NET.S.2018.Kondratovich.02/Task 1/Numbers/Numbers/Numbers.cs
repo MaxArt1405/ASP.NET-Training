18fdaa3397989bb221457a682c7d4709a7a9babc
MaxArt1405 <kondratovich4444@gmail.com>
2018-06-27 23:22:14 +0300

    Task 1

---
+++ NET.S.2018.Kondratovich.02/Task 1/Numbers/Numbers/Numbers.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Numbers
+{
+    public static class Numbers
+    {
+        private static readonly int maxInt = 0x7fffffff;
+        private static readonly int quantityOfBits = 31;
+
+        public static int Insertion(int first, int second, int startPosition, int endPosition)
+        {
+            CheckPosition(startPosition, endPosition);
+
+            int SecondNumber = maxInt >> quantityOfBits - (endPosition - startPosition + 1);
+            SecondNumber &= second;
+            SecondNumber <<= startPosition;
+
+            int Left = maxInt << (endPosition + 1);
+            Left &= first;
+
+            int Right = maxInt >> quantityOfBits - startPosition;
+            Right &= first;
+
+            return Left ^ SecondNumber ^ Right;
+
+        }
+        private static void CheckPosition(int startPosition, int endPosition)
+        {
+            if (startPosition < 0 || startPosition > 31)
+            {
+                throw new ArgumentOutOfRangeException($"The position of {nameof(startPosition)} is incorrect");
+            }
+
+            if (endPosition < 0 || endPosition > 31)
+            {
+                throw new ArgumentOutOfRangeException($"The position of {nameof(endPosition)} is incorrect");
+            }
+
+            if (endPosition < startPosition)
+            {
+                throw new ArgumentException($"The position of {nameof(endPosition)} less than {nameof(startPosition)}");
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.02/Task 2/ArrayExtensions/ArrayExtensionTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ArrayExtensionTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ArrayExtensionTests")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("08f8a3a0-88bd-49dc-9677-6c87e81a5010")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
