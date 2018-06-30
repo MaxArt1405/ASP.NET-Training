c4117455206ccf2e0344fce2168ff998b52462ec
MaxArt1405 <kondratovich4444@gmail.com>
2018-06-30 18:37:40 +0300

    NET.S.2018.Kondratovich.03

---
+++ NET.S.2018.Kondratovich.03/NewtonsNthRoot/FindNthRoot.cs
@@ -0,0 +1,66 @@
+﻿using System;
+
+namespace NewtonsNthRoot
+{
+    public class FindNthRoot
+    {
+        /// <summary>
+        /// Method for searching nth root of real nubmer by Newtons algorithm
+        /// </summary>
+        /// <param name="number">Input real number</param>
+        /// <param name="root">root</param>
+        /// <param name="eps">accurancy</param>
+        /// <returns></returns>
+        public static double SqrtN(double number, int root, double eps)
+        {
+            Valid(number, root, eps);
+
+            double prev = number / root;
+            double next = (1.0 / root) * ((root - 1) * prev + number / Math.Pow(prev, root - 1));
+
+            while (Math.Abs(next - prev) > eps)
+            {
+                prev = next;
+                next = (1.0 / root) * ((root - 1) * prev + number / Math.Pow(prev, root - 1));
+            }
+
+            return next;
+        }
+        /// <summary>
+        /// Method for rizing in a power
+        /// </summary>
+        /// <param name="a">number</param>
+        /// <param name="pow">power</param>
+        /// <returns></returns>
+        private static double Pow(double a, int pow)
+        {
+            double result = 1;
+            for (int i = 0; i < pow; i++)
+            {
+                result *= a;
+            }
+            return result;
+        }
+        /// <summary>
+        /// Method for validating input parameters
+        /// </summary>
+        /// <param name="number">Real input number</param>
+        /// <param name="root">root</param>
+        /// <param name="eps">accurancy</param>
+        private static void Valid(double number, int root, double eps)
+        {
+            if (root < 1)
+            {
+                throw new ArgumentException($"The {nameof(root)} can not be less than 1");
+            }
+            if (root % 2 == 0 && number < 0)
+            {
+                throw new ArgumentException($"It is impossible to extract {nameof(root)} from a negative {nameof(number)}");
+            }
+            if (eps < 0)
+            {
+                throw new ArgumentException($"The accurancy {nameof(eps)} can not be less than 0");
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.03/NewtonsNthRoot/NewtonsNthRoot.Tests.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+using System;
+
+namespace NewtonsNthRoot.Tests
+{
+    [TestFixture]
+    public class FindNthRootTest
+    {
+        [TestCase(1, 5, 0.0001, 1)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.001, 3, 0.0001, 0.1)]
+        [TestCase(0.04100625, 4, 0.0001, 0.45)]
+        [TestCase(8, 3, 0.0001, 2)]
+        [TestCase(0.0279936, 7, 0.0001, 0.6)]
+        [TestCase(0.0081, 4, 0.1, 0.3)]
+        [TestCase(-0.008, 3, 0.1, -0.2)]
+        [TestCase(0.004241979, 9, 0.00000001, 0.545)]
+        public void FindNthRootTestCase(double a, int n, double eps, double expected)
+        {
+            double actual = FindNthRoot.SqrtN(a, n, eps);
+            Assert.AreEqual(actual, expected, eps);
+        }
+
+        [TestCase(-0.01, 2, 0.0001)]
+        [TestCase(0.001, -2, 0.0001)]
+        [TestCase(0.01, 2, -1)]
+        public void FindNthRoot_ThrowArgumentException(double a, int n, double eps)
+        {
+            Assert.Throws<ArgumentException>(() => FindNthRoot.SqrtN(a, n, eps));
+
+        }
+    }
+}
