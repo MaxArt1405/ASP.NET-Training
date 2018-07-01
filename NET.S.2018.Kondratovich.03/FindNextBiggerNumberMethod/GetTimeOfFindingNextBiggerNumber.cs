8cc6a7a42081ed5c5c3d3edecbad0ae8162c19b9
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-01 15:50:43 +0300

    Add files via upload

---
+++ NET.S.2018.Kondratovich.03/FindNextBiggerNumberMethod/GetTimeOfFindingNextBiggerNumber.cs
@@ -0,0 +1,16 @@
+﻿using System.Diagnostics;
+
+namespace FindNextBiggerNumberMethod
+{
+    class GetTimeOfFindingNextBiggerNumber
+    {
+        public static Stopwatch GetLeadTimeFindNextBiggerNumber(int number)
+        {
+            Stopwatch timer = new Stopwatch();
+            timer.Start();
+            FindNextBiggerNumber.FindingNextBiggerNumber(number);
+            timer.Stop();
+            return timer;
+        }
+    }
+}
