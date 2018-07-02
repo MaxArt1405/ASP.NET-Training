bc75e63badf1aaa877a04701601c9c175b9b28a0
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-02 16:57:34 +0300

    Add files via upload

---
+++ NET.S.2018.Kondratovich.03/MethodsForFindingGCD/EuclideanAndSteinMethodForFindingGCD.cs
@@ -0,0 +1,175 @@
+﻿using System;
+
+namespace MethodsForFindingGCD
+{
+    /// <summary>
+    /// Class contains methods to find GCD by Euclidean and Stein algorithms
+    /// </summary>
+    public class EuclideanAndSteinMethodForFindingGCD
+    {
+        /// <summary>
+        /// Euclidean method for two parameters
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        public static int EuclideanMethod(int firstNumber, int secondNumber)
+        {
+            return QuickSolutionForEuclid(firstNumber, secondNumber);
+        }
+        /// <summary>
+        /// Euclidean method for two and more parameters
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <param name="numbers">Array of numbers without first and second</param>
+        /// <returns></returns>
+        public static int EuclideanMethod(int firstNumber, int secondNumber, params int[] numbers)
+        {
+            Valid(numbers);
+            int result = QuickSolutionForEuclid(firstNumber, secondNumber);
+     
+            for(int i = 0; result != 1 && i < numbers.Length; i++)
+            {
+                result = QuickSolutionForEuclid(result, numbers[i]);
+            }
+            return result;
+        }
+        /// <summary>
+        /// Stein method for two parameters
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        public static int SteinMethod(int firstNumber, int secondNumber)
+        {
+            return QuickSolutionForStein(firstNumber, secondNumber);
+        }
+        /// <summary>
+        /// Stein method for two and more parameters
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <param name="numbers">Array of numbers without first and second</param>
+        /// <returns></returns>
+        public static int SteinMethod(int firstNumber, int secondNumber, params int[] numbers)
+        {
+            Valid(numbers);
+            int result = QuickSolutionForStein(firstNumber, secondNumber);
+
+            for (int i = 0; result != 1 && i < numbers.Length; i++)
+            {
+                result = QuickSolutionForStein(result, numbers[i]);
+            }
+            return result;
+        }
+        /// <summary>
+        /// Realization of Euclid GCD algorithm
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        private static int EuclideanMethodEvaluation(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            
+            while (firstNumber != secondNumber)
+            {
+                if (firstNumber > secondNumber)
+                {
+                    firstNumber -= secondNumber;
+                }
+                else
+                {
+                    secondNumber -= firstNumber;
+                }
+            }
+            return firstNumber;
+        }
+        /// <summary>
+        /// Realization of Stein GCD algorithm
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        private static int SteinMethodEvaluation(int firstNumber, int secondNumber)
+        {
+            firstNumber = Math.Abs(firstNumber);
+            secondNumber = Math.Abs(secondNumber);
+            if (firstNumber == secondNumber)
+            {
+                return firstNumber;
+            }
+            if ((~firstNumber & 1) != 0)
+            {
+                if ((secondNumber & 1) != 0)
+                {
+                    return SteinMethodEvaluation(firstNumber >> 1, secondNumber);
+                }
+                else
+                {
+                    return SteinMethodEvaluation(firstNumber >> 1, secondNumber >> 1) << 1;
+                }
+            }
+            if ((~secondNumber & 1) != 0)
+            {
+                return SteinMethodEvaluation(firstNumber, secondNumber >> 1);
+            }
+
+            if (firstNumber > secondNumber)
+            {
+                return SteinMethodEvaluation((firstNumber - secondNumber) >> 1, secondNumber);
+            }
+            return SteinMethodEvaluation((secondNumber - firstNumber) >> 1, firstNumber);
+        }
+        /// <summary>
+        /// Quick solution for Euclid Method(if one of the numbers equals zero, it's useless to evaluate)
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        private static int QuickSolutionForEuclid(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0)
+            {
+                return Math.Abs(secondNumber);
+            }
+            if(secondNumber == 0)
+            {
+                return Math.Abs(firstNumber);
+            }  
+            return EuclideanMethodEvaluation(firstNumber, secondNumber);
+        }
+        /// <summary>
+        /// Quick solution for Stein Method(if one of the numbers equals zero, it's useless to evaluate)
+        /// </summary>
+        /// <param name="firstNumber">First number</param>
+        /// <param name="secondNumber">Second number</param>
+        /// <returns></returns>
+        private static int QuickSolutionForStein(int firstNumber, int secondNumber)
+        {
+            if (firstNumber == 0)
+            {
+                return Math.Abs(secondNumber);
+            }
+            if (secondNumber == 0)
+            {
+                return Math.Abs(firstNumber);
+            }
+            return SteinMethodEvaluation(firstNumber, secondNumber);
+        }
+        /// <summary>
+        /// Validation method for array of parameters
+        /// </summary>
+        /// <exception cref="ArgumentNullException">Throws, when ref of array is null</exception>
+        /// <param name="numbers">Array of parameters</param>
+        private static void Valid(int[] numbers)
+        {
+            if (numbers == null)
+            {
+                throw new ArgumentNullException($"The value of {nameof(numbers)} can not be not null");
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.03/MethodsForFindingGCD/TestForEuclideanMethod.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using MethodsForFindingGCD;
+using NUnit.Framework;
+
+namespace MethodForFindingGCDTests
+{
+    [TestFixture]
+    public class TestForEuclideanMethod
+    {
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, -5, ExpectedResult = 5)]
+        public int TestingEuclideanMethodForTwoParams(int a, int b)
+        {
+            return EuclideanAndSteinMethodForFindingGCD.EuclideanMethod(a, b);
+        }
+
+        [TestCase(-13, 25, ExpectedResult = 1)]
+        [TestCase(4, 16, 8, ExpectedResult = 4)]
+        [TestCase(5, 40, 20, 10, ExpectedResult = 5)]
+        [TestCase(24, 50, 72, 84, 92, ExpectedResult = 2)]
+        [TestCase(-12, 24, -48, 96, -192, 384, ExpectedResult = 12)]
+        [TestCase(13, 14, 25, 26, 32, 44, 46, 52, ExpectedResult = 1)]
+        public int TestingEuclideanMethodForThreeAndMoreParams(int a, int b, params int[] numbers)
+        {
+            return EuclideanAndSteinMethodForFindingGCD.EuclideanMethod(a, b, numbers);
+        }
+
+        [Test]
+        public void Euclidean_NullArgument_ArgumentsNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => EuclideanAndSteinMethodForFindingGCD.EuclideanMethod(4, 16, null));
+        }
+    }
+    [TestFixture]
+    public class TestForSteinMethod
+    {
+        [TestCase(1, 10, ExpectedResult = 1)]
+        [TestCase(5, 10, ExpectedResult = 5)]
+        [TestCase(24, 24, ExpectedResult = 24)]
+        [TestCase(-5, 10, ExpectedResult = 5)]
+        [TestCase(5, 0, ExpectedResult = 5)]
+        [TestCase(0, 0, ExpectedResult = 0)]
+        [TestCase(0, -5, ExpectedResult = 5)]
+        public int TestingEuclideanMethodForTwoParams(int a, int b)
+        {
+            return EuclideanAndSteinMethodForFindingGCD.SteinMethod(a, b);
+        }
+
+        [TestCase(-13, 25, ExpectedResult = 1)]
+        [TestCase(4, 16, 8, ExpectedResult = 4)]
+        [TestCase(5, 40, 20, 10, ExpectedResult = 5)]
+        [TestCase(24, 50, 72, 84, 92, ExpectedResult = 2)]
+        [TestCase(-12, 24, -48, 96, -192, 384, ExpectedResult = 12)]
+        [TestCase(13, 14, 25, 26, 32, 44, 46, 52, ExpectedResult = 1)]
+        public int TestingEuclideanMethodForThreeAndMoreParams(int a, int b, params int[] numbers)
+        {
+            return EuclideanAndSteinMethodForFindingGCD.SteinMethod(a, b, numbers);
+        }
+        [Test]
+        public void Euclidean_NullArgument_ArgumentsNullException()
+        {
+            Assert.Throws<ArgumentNullException>(() => EuclideanAndSteinMethodForFindingGCD.SteinMethod(4, 16, null));
+        }
+    }
+}
