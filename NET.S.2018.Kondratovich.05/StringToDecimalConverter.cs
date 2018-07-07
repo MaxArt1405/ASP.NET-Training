3a10ec5bfca61f59eb5b768258fa451d0b28f32a
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-07 17:48:22 +0300

    Day 5

---
+++ NET.S.2018.Kondratovich.05/StringToDecimalConverter.cs
@@ -0,0 +1,57 @@
+﻿using System;
+
+namespace DecimalConverter
+{
+    public static class StringToDecimalConverter
+    {
+        /// <summary>
+        /// Method for converting string to any notation from 2 to 16
+        /// </summary>
+        /// <param name="source">Input string</param>
+        /// <param name="notation">The notation for converting</param>
+        /// <returns></returns>
+        public static int ConvertingToDecimal(this string source, Notation notation)
+        {
+            Valid(source, notation);
+            return GetConvertedNumber(source, notation);
+        }
+        /// <summary>
+        /// Method, which return converted string to recieved notation
+        /// </summary>
+        /// <param name="source">Input string</param>
+        /// <param name="notation">The notation for converting</param>
+        /// <returns></returns>
+        private static int GetConvertedNumber(this string source, Notation notation)
+        {
+            int result = 0;
+            for (int i = 0, j = source.Length - 1; i < source.Length; i++)
+            {
+                result += (int)Math.Pow(notation.Scale, j - i) * notation.Alphabet.IndexOf(source[i].ToString().ToUpper());
+            }
+            return result;
+        }
+        /// <summary>
+        /// Method for validating input data
+        /// </summary>
+        /// <param name="source">Input string number</param>
+        /// <param name="notation">The notation for converting</param>
+        /// <exception cref="ArgumentNullException">Throws when input source is null or empty.</exception>    
+        /// <exception cref="ArgumentException">Throws when scale of notation isn't from 2 to 16 .</exception>
+        /// <exception cref="OverflowException">Throws when the source.Length is greater than capacity of Int32.</exception>
+        private static void Valid(this string source, Notation notation)
+        {
+            if (string.IsNullOrEmpty(source))
+            {
+                throw new ArgumentNullException($"{nameof(source)} is empty or null.");
+            }
+            if (notation.Scale < 2 || notation.Scale > 16)
+            {
+                throw new ArgumentException($"The {nameof(notation.Scale)} of an notation must be from 2 to 16.");
+            }
+            if (source.Length >= sizeof(int) * 8)
+            {
+                throw new OverflowException($"Length of {nameof(source)} should be less than 32!");
+            }
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.05/StringToDecimalConverterTest.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using NUnit.Framework;
+
+namespace DecimalConverter.Test
+{
+    [TestFixture]
+    public class StringToDecimalConverterTest
+    {
+        [TestCase("0110111101100001100001010111111", 2, ExpectedResult = 934331071)]
+        [TestCase("01101111011001100001010111111", 2, ExpectedResult = 233620159)]
+        [TestCase("11101101111011001100001010", 2, ExpectedResult = 62370570)]
+        [TestCase("1AeF101", 16, ExpectedResult = 28242177)]
+        [TestCase("1ACB67", 16, ExpectedResult = 1756007)]
+        [TestCase("764241", 8, ExpectedResult = 256161)]
+        [TestCase("10", 5, ExpectedResult = 5)]
+        public int ConvertingStringToDecimalTest(string number, int scale)
+        {
+            Notation notation = new Notation(scale);
+            return StringToDecimalConverter.ConvertingToDecimal(number, notation);
+        }
+
+        [TestCase("764241", 20)]
+        public void StringToDecimalConverterTest_ThrowsArgumentException(string number, int scale)
+        {
+            Notation notation = new Notation(scale);
+            Assert.Throws<ArgumentException>(() => StringToDecimalConverter.ConvertingToDecimal(number, notation));
+        }
+
+        [TestCase(null, 2)]
+        [TestCase("", 10)]
+        public void StringToDecimalConverterTest_ThrowsNullException(string number, int scale)
+        {
+            Notation notation = new Notation(scale);
+            Assert.Throws<ArgumentNullException>(() => StringToDecimalConverter.ConvertingToDecimal(number, notation));
+        }
+
+        [TestCase("100000000000000111000000000000011100000", 2)]
+        [TestCase("10101010111101010101010101010101010101011", 2)]
+        public void StringToDecimalConverterTest_ThrowsOverflowException(string number, int scale)
+        {
+            Notation notation = new Notation(scale);
+            Assert.Throws<OverflowException>(() => StringToDecimalConverter.ConvertingToDecimal(number, notation));
+        }
+    }
+}
