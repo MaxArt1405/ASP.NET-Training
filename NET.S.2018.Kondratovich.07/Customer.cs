86a7f7312f35e30668575538e934dabf19b78064
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-11 16:23:53 +0300

    Add files via upload

--- NET.S.2018.Kondratovich.07/Customer.cs
+++ NET.S.2018.Kondratovich.07/Customer.cs
@@ -5,24 +5,44 @@ namespace NET.S._2018.Kondratovich._07
 {
     public class Customer : IFormattable
     {
+        /// <summary>
+        /// String property Name.
+        /// </summary>
         public string Name { get; set; }
-
+        /// <summary>
+        /// String property Contact Phone.
+        /// </summary>
         public string ContactPhone { get; set; }
-
+        /// <summary>
+        /// Decimal property Revenue.
+        /// </summary>
         public decimal Revenue { get; set; }
-
+        /// <summary>
+        /// Constructor with parameters.
+        /// </summary>
+        /// <param name="name">Name of the customer.</param>
+        /// <param name="phone">Contact phone of the customer.</param>
+        /// <param name="revenue">Revenue of the customer.</param>
         public Customer(string name, string contactPhone, decimal revenue)
         {
             Name = name;
             ContactPhone = contactPhone;
             Revenue = revenue;
         }
-
+        /// <summary>
+        /// Overriding method converts Customer object into string representation with general format.
+        /// </summary>
+        /// <returns>String representation of the customer.</returns>
         public override string ToString()
         {
             return string.Format("Customer record: {0}, {1}, {2}", Name, ContactPhone, Revenue);
         }
-
+        /// <summary>
+        /// Converts Customer object into string representation.
+        /// </summary>
+        /// <param name="format">Format of string representation.</param>
+        /// <param name="formatProvider">Format provider.</param>
+        /// <returns>String representation of the customer.</returns>
         public string ToString(string format, IFormatProvider formatProvider = null)
         {
             if (String.IsNullOrEmpty(format))
@@ -45,6 +65,12 @@ namespace NET.S._2018.Kondratovich._07
                     return string.Format(formatProvider, "Customer record: {0}", ContactPhone);
                 case "REVENUE":
                     return string.Format(formatProvider, "Customer record: {0}", Revenue);
+                case "NAMEREVENUE":
+                    return string.Format(formatProvider, "Customer record: {0}, {1}", Name, Revenue);
+                case "CONTACTREVENUE":
+                    return string.Format(formatProvider, "Customer record: {0}, {1}", ContactPhone, Revenue);
+                case "NAMECONTACT":
+                    return string.Format(formatProvider, "Customer record: {0}, {1}", Name, ContactPhone);
                 default:
                     throw new FormatException(string.Format("The {0} format string is not supported.", format));
             }
---
+++ NET.S.2018.Kondratovich.07/CustomersFormatter.cs
@@ -0,0 +1,70 @@
+﻿using System;
+using System.Globalization;
+
+namespace NET.S._2018.Kondratovich._07
+{
+    public class CustomersFormatter : ICustomFormatter, IFormatProvider
+    {
+        private readonly IFormatProvider parentFormatProvider;
+        /// <summary>
+        /// Constructor without parameters.
+        /// </summary>
+        public CustomersFormatter() : this(CultureInfo.CurrentCulture) { }
+        /// <summary>
+        /// Ctor with one parameter.
+        /// </summary>
+        /// <param name="parent">Format provider.</param>
+        public CustomersFormatter(IFormatProvider parent)
+        {
+            parentFormatProvider = parent;
+        }
+        /// <summary>
+        /// Converts Customer object into string representation using additional formats.
+        /// </summary>
+        /// <param name="format">Format of string representation.</param>
+        /// <param name="obj">Customer object for converting.</param>
+        /// <param name="formatProvider">Format provider.</param>
+        /// <returns>String representation of the customer.</returns>
+        public string Format(string format, object arg, IFormatProvider formatProvider)
+        {
+            Customer customer = arg as Customer;
+            if (customer is null)
+            {
+                throw new ArgumentNullException($"The {nameof(customer)} is null.");
+            }
+
+            if (string.IsNullOrEmpty(format))
+            {
+                format = "FULL+";
+            }
+
+            if (formatProvider is null)
+            {
+                formatProvider = parentFormatProvider;
+            }
+
+            switch (format.ToUpper())
+            {
+                case "FULL+":
+                    return $"Customer record: Name: {customer.Name}, Revenue: {customer.Revenue}, Phone: {customer.ContactPhone}";
+                case "NAME+":
+                    return $"Customer record name: {customer.Name}";
+                case "CONTACT+":
+                    return $"Customer record contact phone: {customer.ContactPhone}";
+                case "REVENUE+":
+                    return $"Customer record revenue: {customer.Revenue}";
+                default:
+                    throw new FormatException($"This format {format} is not supported.");
+            }
+        }
+
+        public object GetFormat(Type formatType)
+        {
+            if (formatType == typeof(ICustomFormatter))
+            {
+                return this;
+            }
+            return null;
+        }
+    }
+}
