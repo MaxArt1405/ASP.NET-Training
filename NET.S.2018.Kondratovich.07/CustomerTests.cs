9d8eaad8f0752f458a7a92680ea391709dad3fc6
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-12 20:51:23 +0300

    Add files via upload

--- NET.S.2018.Kondratovich.07/CustomerTests.cs
+++ NET.S.2018.Kondratovich.07/CustomerTests.cs
@@ -8,25 +8,26 @@ namespace Customer.Tests
     public class CustomerTests
     {
         private readonly NET.S._2018.Kondratovich._07.Customer customer = 
-            new NET.S._2018.Kondratovich._07.Customer("Jeffrey Richter", "+1 (425) 555-0100", 1000000);
+            new NET.S._2018.Kondratovich._07.Customer("Jeffrey Richter", "+375 (29) 268-0859", 2145854);
 
-        [TestCase("Full", ExpectedResult = "Customer record: Jeffrey Richter, +1 (425) 555-0100, 1000000")]
+        [TestCase("Full", ExpectedResult = "Customer record: Jeffrey Richter, +375 (29) 268-0859, 2,145,854.00")]
         [TestCase("Name", ExpectedResult = "Customer record: Jeffrey Richter")]
-        [TestCase("Contact", ExpectedResult = "Customer record: +1 (425) 555-0100")]
-        [TestCase("Revenue", ExpectedResult = "Customer record: 1000000")]
-        [TestCase("NameRevenue", ExpectedResult = "Customer record: Jeffrey Richter, 1000000")]
-        [TestCase("ContactRevenue", ExpectedResult = "Customer record: +1 (425) 555-0100, 1000000")]
-        [TestCase("NameContact", ExpectedResult = "Customer record: Jeffrey Richter, +1 (425) 555-0100")]
+        [TestCase("Contact", ExpectedResult = "Customer record: +375 (29) 268-0859")]
+        [TestCase("Revenue", ExpectedResult = "Customer record: 2,145,854.00")]
+        [TestCase("NameRevenue", ExpectedResult = "Customer record: Jeffrey Richter, 2,145,854.00")]
+        [TestCase("ContactRevenue", ExpectedResult = "Customer record: +375 (29) 268-0859, 2,145,854.00")]
+        [TestCase("NameContact", ExpectedResult = "Customer record: Jeffrey Richter, +375 (29) 268-0859")]
         public string CustomerReprezentations_SuccesfullTest(string format)
         {
             return customer.ToString(format);
         }
 
-        [TestCase("Full+", null, ExpectedResult = "Customer record: Name: Jeffrey Richter, Revenue: 1000000, Phone: +1 (425) 555-0100")]
-        [TestCase("Name+", null, ExpectedResult = "Customers name: Jeffrey Richter")]
-        [TestCase("Contact+", null, ExpectedResult = "Customers record contact phone: +1 (425) 555-0100")]
-        [TestCase("Revenue+", null, ExpectedResult = "Customers revenue: 1000000")]
-         public string Custom_CustomerReprezentations_SuccesfullTest(string format, IFormatProvider formatProvider)
+        [TestCase("Full+", null, ExpectedResult = "Customer record: Name: Jeffrey Richter, Revenue: 2,145,854.00, Phone: +375 (29) 268-0859")]
+        [TestCase("Name+", null, ExpectedResult = "Customer record name: Jeffrey Richter")]
+        [TestCase("Contact+", null, ExpectedResult = "Customer record contact phone: +375 (29) 268-0859")]
+        [TestCase("Revenue+", null, ExpectedResult = "Customer record revenue: 2,145,854.00")]
+        [TestCase("ByWord+", null, ExpectedResult = "Customer record: Revenue - two one four five eight five four")]
+        public string Custom_CustomerReprezentations_SuccesfullTest(string format, IFormatProvider formatProvider)
         {
             CustomersFormatter custom = new CustomersFormatter();
             return custom.Format(format, customer, formatProvider);
