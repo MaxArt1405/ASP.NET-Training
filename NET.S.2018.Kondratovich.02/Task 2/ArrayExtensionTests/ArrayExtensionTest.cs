f7e174d9688ba4e35c1c73dbe59acea10127515b
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-12 22:07:46 +0300

    Update ArrayExtensionTest.cs

--- NET.S.2018.Kondratovich.02/Task 2/ArrayExtensionTests/ArrayExtensionTest.cs
+++ NET.S.2018.Kondratovich.02/Task 2/ArrayExtensionTests/ArrayExtensionTest.cs
@@ -1,119 +1,35 @@
-﻿using System;
-using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+using NUnit.Framework;
 using ArrayExtensions;
 using static ArrayExtensions.ArrayExtension;
 
 namespace ArrayExtensionTests
 {
-    [TestClass]
+    [TestFixture]
     public class ArrayExtensionTest
     {
-        [TestMethod]
-        public void FilterNumbers_WithDigit0_Success()
+        [TestCase(0, new int[] { 1, 10, 8, 20, 11, 60}, ExpectedResult = new [] { 10, 20, 60 })]
+        [TestCase(1, new int[] { 1, 2, 5, 10, 4, 11 }, ExpectedResult = new[] { 1, 10, 11 })]
+        [TestCase(2, new int[] { 1, 2, 5, 8, 12, 45 }, ExpectedResult = new[] { 2, 12 })]
+        [TestCase(3, new int[] { 1, 2, 3, 6, 13 }, ExpectedResult = new[] { 3, 13 })]
+        [TestCase(4, new int[] { 1, 2, 4, 8, 14, 24 }, ExpectedResult = new[] { 4, 14, 24 })]
+        [TestCase(5, new int[] { 51, 2, 5, 7, 751 }, ExpectedResult = new[] { 51, 5, 751 })]
+        [TestCase(6, new int[] { 1, 2, 6, 12, 16, 26 }, ExpectedResult = new[] { 6, 16, 26 })]
+        [TestCase(7, new int[] { 2, 7, 14, 27, 52, 77 }, ExpectedResult = new[] { 7, 27, 77 })]
+        [TestCase(8, new int[] { 1, 5, 8, 15, 18 }, ExpectedResult = new[] { 8, 18 })]
+        [TestCase(9, new int[] { 1, 4, 8, 9, 15, 19 }, ExpectedResult = new[] { 9, 19 })]
+        public int[] FilterNumbers_WithDigit0_Success(int digit, int[] array )
         {
-            int[] array = new int[] { 1, 10, 5, 15, 20, 58, 60 };
-            int[] expected = new int[] { 10, 20, 60 };
-            int[] actual = array.FilterNumbers(new PredicateDigit0());
-            CollectionAssert.AreEqual(expected, actual);
+            int[] actual = array.FilterNumbers(new PredicateDigit(digit));
+            return actual;
         }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit1_Success()
+        [Test]
+        public void FilterNumbers_ThrowsNullArgumentException()
         {
-            int[] array = new int[] { 1, 2, 5, 10, 4, 11 };
-            int[] expected = new int[] { 1, 10, 11 };
-            int[] actual = array.FilterNumbers(new PredicateDigit1());
-            CollectionAssert.AreEqual(expected, actual);
+            int digit = 1;
+            int[] actual = null;
+            Assert.Throws<ArgumentNullException>(() => actual.FilterNumbers(new PredicateDigit(digit)));
         }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit2_Success()
-        {
-            int[] array = new int[] { 2, 4, 6, 8, 10, 12 };
-            int[] expected = new int[] { 2, 12 };
-            int[] actual = array.FilterNumbers(new PredicateDigit2());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit3_Success()
-        {
-            int[] array = new int[] { 1, 2, 3, 6, 13 };
-            int[] expected = new int[] { 3, 13 };
-            int[] actual = array.FilterNumbers(new PredicateDigit3());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit4_Success()
-        {
-            int[] array = new int[] { 1, 2, 4, 8, 14, 24 };
-            int[] expected = new int[] { 4, 14, 24 };
-            int[] actual = array.FilterNumbers(new PredicateDigit4());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit5_Success()
-        {
-            int[] array = new int[] { 51, 2, 5, 7, 751 };
-            int[] expected = new int[] { 51, 5, 751 };
-            int[] actual = array.FilterNumbers(new PredicateDigit5());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit6_Success()
-        {
-            int[] array = new int[] { 1, 2, 6, 12, 16, 26 };
-            int[] expected = new int[] { 6, 16, 26 };
-            int[] actual = array.FilterNumbers(new PredicateDigit6());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit7_Success()
-        {
-            int[] array = new int[] { 2, 7, 14, 27, 52, 77  };
-            int[] expected = new int[] { 7, 27, 77 };
-            int[] actual = array.FilterNumbers(new PredicateDigit7());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit8_Success()
-        {
-            int[] array = new int[] { 1, 5, 8, 15, 18 };
-            int[] expected = new int[] { 8, 18 };
-            int[] actual = array.FilterNumbers(new PredicateDigit8());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        public void FilterNumbers_WithDigit9_Success()
-        {
-            int[] array = new int[] { 1, 4, 8, 9, 15, 19 };
-            int[] expected = new int[] { 9, 19 };
-            int[] actual = array.FilterNumbers(new PredicateDigit9());
-            CollectionAssert.AreEqual(expected, actual);
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentException))]
-        public void FilterNumbers_WithEmptyArgument_ThrowArgumentException()
-        {
-            int[] array = new int[] { };
-            int[] actual = array.FilterNumbers(new PredicateDigit0());
-        }
-
-        [TestMethod]
-        [ExpectedException(typeof(ArgumentNullException))]
-        public void FilterNumbers_WithNullArgument_ThrowArgumentNulException()
-        {
-            int[] array = null;
-            int[] actual = array.FilterNumbers(new PredicateDigit0());
-        }
-
     }
 }
    
