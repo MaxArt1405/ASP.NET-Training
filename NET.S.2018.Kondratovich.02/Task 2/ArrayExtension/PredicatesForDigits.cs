4c730329c9e1c8352c5e63e8ac199d17e37ffa55
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-12 22:06:23 +0300

    Update PredicatesForDigits.cs

--- NET.S.2018.Kondratovich.02/Task 2/ArrayExtension/PredicatesForDigits.cs
+++ NET.S.2018.Kondratovich.02/Task 2/ArrayExtension/PredicatesForDigits.cs
@@ -2,15 +2,25 @@
 
 namespace ArrayExtensions
 {
+    /// <summary>
+    /// Class for searching criteria
+    /// </summary>
     public class PredicateDigit
     {
         private int _digit;
-
+        /// <summary>
+        /// Constructor with one parameter
+        /// </summary>
+        /// <param name="digit">Searching digit</param>
         public PredicateDigit(int digit)
         {
             _digit = digit;
         }
-
+        /// <summary>
+        /// Method for answer(is there this digit)
+        /// </summary>
+        /// <param name="number">Input number</param>
+        /// <returns></returns>
         public bool IsDigit(int number)
         {
             if (_digit != 0)
