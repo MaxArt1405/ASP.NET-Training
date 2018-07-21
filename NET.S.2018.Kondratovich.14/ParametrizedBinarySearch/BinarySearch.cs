75e3f57824d4fa132b864041400bd314bba52549
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-21 14:52:10 +0300

    Add Parametrized Binary Search

---
+++ NET.S.2018.Kondratovich.14/ParametrizedBinarySearch/BinarySearch.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinarySearchParametrized
+{
+    public class BinarySearch
+    {
+        /// <summary>
+        /// Search element in T[]array by binary search.
+        /// To compare elements use <see cref="Comparer<T>"/>
+        /// </summary>
+        /// <param name="array">Array to search in.</param>
+        /// <param name="element">Element to search.</param>
+        /// <exception cref="ArgumentNullException">
+        /// If <paramref name="array"/> is null.
+        /// If <paramref name="element"/> is null.
+        /// </exception>
+        /// <exception cref="ArgumentException">
+        /// If T-type does not impelemnts <see cref="IComparer{T}"/>.
+        /// </exception>
+        /// <returns>Found index. If element not found then return -1.</returns>
+        public static int Search<T>(T[] array, T element)
+        {
+            if (array == null)
+            {
+                throw new ArgumentNullException(nameof(array));
+            }
+
+            if (element == null)
+            {
+                throw new ArgumentNullException(nameof(element));
+            }
+
+            if (Comparer<T>.Default == null)
+            {
+                throw new ArithmeticException($"{nameof(element)} must implementation IComparer");
+            }
+
+            return InnerBinarySearch(array, element, Comparer<T>.Default, 0, array.Length - 1);
+        }
+
+        private static int InnerBinarySearch<T>(T[] array, T element, Comparer<T> comparer, int start, int end)
+        {
+            while (start <= end)
+            {
+                int middle = start + ((end - start) >> 1);
+                int resultCompare = comparer.Compare(array[middle], element);
+
+                if (resultCompare == 0)
+                {
+                    return middle;
+                }
+                if (resultCompare > 0)
+                {
+                    end = middle - 1;
+                }
+                if (resultCompare < 0)
+                {
+                    start = middle + 1;
+                }
+            }
+            return -1;
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.14/ParametrizedBinarySearch/BinarySearchTesting.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using BinarySearchParametrized;
+using NUnit.Framework;
+
+namespace BinarySearchTest
+{
+    [TestFixture]
+    public class BinarySearchTesting
+    {
+        [Test]
+        public void BinarySearchForElement_Success()
+        {
+            int[] number = { 0, 1, 2, 4, 6, 8, 10, 12, 14, 16, 18, 20, 22, 24, 26 };
+            Assert.AreEqual(BinarySearch.Search(number, 14), 8);
+        }
+        [Test]
+        public void BinarySearchInEmptyArray_Succed()
+        {
+            int[] number = { };
+            Assert.AreEqual(BinarySearch.Search(number, 5), -1);
+        }
+        [Test]
+        public void BinarySearchForElement_NotFound_Success()
+        {
+            int[] number = { 1, 2, 3, 4 };
+            Assert.AreEqual(BinarySearch.Search(number, 5), -1);
+        }
+        [TestCase("ABCDEFGHIJKLMOPRSTUVWXYZ",'Z', ExpectedResult = 23)]
+        [TestCase("ABCDEFGHIJKLMOPRSTUVWXYZ", 'k', ExpectedResult = 10)]
+        [TestCase("ABCDEFGHIJKLMOPRSTUVWXYZ", 'B', ExpectedResult = 1)]
+        [TestCase("ABCDEFGHIJKLMOPRSTUVWXYZ", 'w', ExpectedResult = 20)]
+        public int BinarySearch_CustomComparer_Succed(string input, char item)
+        {
+            char[] str = input.ToCharArray();
+            return BinarySearch.Search(str, char.ToUpper(item));
+        }
+    }
+}
