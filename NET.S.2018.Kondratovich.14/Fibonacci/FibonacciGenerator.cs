57e1c6a4ab28d69837d8c04bf320b23ba47be78c
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-21 15:16:31 +0300

    Add Fibonacci

---
+++ NET.S.2018.Kondratovich.14/Fibonacci/FibonacciGenerator.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fibonacci
+{
+    public class FibonacciGenerator
+    {
+        public static IEnumerable<int> GenerateFibonacci(int count)
+        {
+            int first = 0;
+            int second = 1;
+
+            for(int i = 0; i < count; i++)
+            {
+                yield return first;
+                NextStep(ref first, ref second);
+            }
+        }
+        private static int NextStep(ref int first,ref int second)
+        {
+            var nextNumber = first + second;
+            first = second;
+            second = nextNumber;
+            return nextNumber;
+        }
+    }
+}
---
+++ NET.S.2018.Kondratovich.14/Fibonacci/FibonacciTests.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Linq;
+using Fibonacci;
+using NUnit.Framework;
+
+namespace FibnacciTest
+{
+    [TestFixture]
+    public class FibonacciTests
+    {
+        [Test]
+        [TestCase(0, new int[] { })]
+        [TestCase(6, new int[] { 0, 1, 1, 2, 3, 5 })]
+        [TestCase(12, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 })]
+        [TestCase(18, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 })]
+        [TestCase(24 ,new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657 })]
+        public void FibonacciTestSuccess(int count, int[] expected)
+        {
+            Assert.AreEqual(FibonacciGenerator.GenerateFibonacci(count), expected);
+        }
+    }
+}
