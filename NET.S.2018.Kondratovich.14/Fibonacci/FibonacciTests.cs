5a258b5f991191e1541e130e078dc8438c730d1e
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-23 20:16:28 +0300

    Add files via upload

--- NET.S.2018.Kondratovich.14/Fibonacci/FibonacciTests.cs
+++ NET.S.2018.Kondratovich.14/Fibonacci/FibonacciTests.cs
@@ -1,6 +1,4 @@
-﻿using System;
-using System.Linq;
-using System.Numerics;
+﻿using System.Numerics;
 using Fibonacci;
 using NUnit.Framework;
 
@@ -9,14 +7,11 @@ namespace FibnacciTest
     [TestFixture]
     public class FibonacciTests
     {
-        [TestCase(0, new int[] {})]
-        [TestCase(6, new int[] { 0, 1, 1, 2, 3, 5 })]
-        [TestCase(12, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 })]
-        [TestCase(18, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 })]
-        [TestCase(24, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657 })]
-        public void FibonacciTestSuccess(int count, int[] expected)
+        private readonly BigInteger[] expected = new BigInteger[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657, 46368, 75025, 121393, 196418, 317811, 514229, 832040, 1346269, 2178309, 3524578, 5702887, 9227465, 14930352, 24157817, 39088169, 63245986, 102334155 };
+        [Test]
+        public void FibonacciTestSuccess()
         {
-            Assert.AreEqual(FibonacciGenerator.GenerateFibonacci(count), expected);
+            CollectionAssert.AreEqual(FibonacciGenerator.GenerateFibonacci(expected.Length),expected);
         }
     }
 }
