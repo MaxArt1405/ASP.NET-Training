e8e86700d941ff30c2c85943211aaceccf4b92f4
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-23 18:54:53 +0300

    Add files via upload

--- NET.S.2018.Kondratovich.14/Fibonacci/FibonacciGenerator.cs
+++ NET.S.2018.Kondratovich.14/Fibonacci/FibonacciGenerator.cs
@@ -1,19 +1,16 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using System.Collections.Generic;
+using System.Numerics;
 
 namespace Fibonacci
 {
     public class FibonacciGenerator
     {
-       /* public static IEnumerable<int> GenerateFibonacci(int count)
+        public static IEnumerable<BigInteger> GenerateFibonacci(int count)
         {
-            int first = 0;
-            int second = 1;
+            BigInteger first = 0;
+            BigInteger second = 1;
 
-            int NextStep(int firstNumber, int secondNumber)
+            BigInteger NextNumber()
             {
                 var nextNumber = first + second;
                 first = second;
@@ -24,28 +21,8 @@ namespace Fibonacci
             for (int i = 0; i < count; i++)
             {
                 yield return first;
-                NextStep(first, second);
-            }
-        }*/
-        public static IEnumerable<int> GenerateFibonacci()
-        {
-            int first = 0;
-            int second = 1;
-
-            int NextNumber()
-            {
-                var nextNumber = first + second;
-                first = second;
-                second = nextNumber;
-                return nextNumber;
-            }
-
-            for (int i = 0; i < int.MaxValue - second; i++)
-            {
-                yield return first;
                 NextNumber();
             }
-        } 
-         
+        }
     }
 }
--- NET.S.2018.Kondratovich.14/Fibonacci/FibonacciTests.cs
+++ NET.S.2018.Kondratovich.14/Fibonacci/FibonacciTests.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Linq;
+using System.Numerics;
 using Fibonacci;
 using NUnit.Framework;
 
@@ -8,16 +9,14 @@ namespace FibnacciTest
     [TestFixture]
     public class FibonacciTests
     {
-        [Test]
-        [TestCase(0, new int[] { })]
+        [TestCase(0, new int[] {})]
         [TestCase(6, new int[] { 0, 1, 1, 2, 3, 5 })]
         [TestCase(12, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89 })]
         [TestCase(18, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597 })]
-        [TestCase(24 ,new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657 })]
+        [TestCase(24, new int[] { 0, 1, 1, 2, 3, 5, 8, 13, 21, 34, 55, 89, 144, 233, 377, 610, 987, 1597, 2584, 4181, 6765, 10946, 17711, 28657 })]
         public void FibonacciTestSuccess(int count, int[] expected)
         {
-           // Assert.AreEqual(FibonacciGenerator.GenerateFibonacci(count), expected);
-             Assert.AreEqual(FibonacciGenerator.GenerateFibonacci().Take(count), expected);
+            Assert.AreEqual(FibonacciGenerator.GenerateFibonacci(count), expected);
         }
     }
 }
