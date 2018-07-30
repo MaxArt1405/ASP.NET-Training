b18c67f59d06ef612391f4354ca501a0e417772a
MaxArt1405 <kondratovich4444@gmail.com>
2018-07-30 19:46:19 +0300

    Skeleton of BinaryTree solution

---
+++ NET.S.2018.Kondratovich.15/BinaryTree/BinarySearchTree.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    public class BinarySearchTree<T> where T : IComparable<T>
+    { 
+   
+        private Knot<T> _root;
+        private IComparer<T> _comparer;
+
+
+        public BinarySearchTree(){}
+        public BinarySearchTree(IComparer<T> comparer)
+        {
+            _comparer = comparer ?? throw new ArgumentNullException($"The {nameof(comparer)} is null");
+        }
+        public BinarySearchTree(IComparer<T> comparer, IEnumerable<T> collection)
+        {
+            _comparer = comparer ?? throw new ArgumentNullException($"The {nameof(comparer)} is null");
+
+            foreach (var i in collection)
+            {
+                Add(i);
+            }
+        }
+
+        public void Add(T value) { }
+        public void Remove(T value){ }
+
+        private int Count { get; set; }
+
+        private bool IsEmpty => Count == 0;
+    }
+}
---
+++ NET.S.2018.Kondratovich.15/BinaryTree/Knot.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BinaryTree
+{
+    internal class Knot<T>
+    {
+        public Knot(Knot<T> left, Knot<T> rigth, T value)
+        {
+            Left = left;
+            Rigth = rigth;
+            Value = value;
+        }
+
+        public Knot<T> Left { get; set; }
+
+        public Knot<T> Rigth { get; set; }
+
+        public T Value { get; set; }
+    }
+}
